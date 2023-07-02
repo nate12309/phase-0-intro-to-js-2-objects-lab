7a0017b544e5a1a56d32c0bf187a68f864cb32e5
Nate <nate12309@LAPTOP-CNADA5S6.localdomain>
2023-07-01 20:37:34 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+let employee = { name: "John", age: 30, role: "Manager" };
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const updatedEmployee = { ...employee, [key]: value };
+  return updatedEmployee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const employeeClone = { ...employee };
+  delete employeeClone[key];
+  return employeeClone;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
